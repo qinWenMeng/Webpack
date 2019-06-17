a9d7d4565e7ae9c4f57687d19dc5cff413c56eb9
qinWenMeng <qwm668@gmail.com>
2019-06-17 23:48:37 +0800

    --切换配置项

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -5,7 +5,7 @@ const webpack = require('webpack');
 // console.log('🎈 🎈 🎈', require('clean-webpack-plugin'));
 
 module.exports = {
-  /* mode: 'development', // 开发模式,打包出来的bundle不压缩代码
+  mode: 'development', // 开发模式,打包出来的bundle不压缩代码
   optimization: {
     usedExports: true // 打包出来的bundle里注释 导出但未引用的 export
   },
@@ -13,8 +13,8 @@ module.exports = {
   devServer: {
     contentBase: './dist',
     hot: true
-  }, */
-  mode: 'production', // 删除的“未引用代码(dead code)” 压缩代码
+  },
+  // mode: 'production', // 删除的“未引用代码(dead code)” 压缩代码
   // entry: './src/index.js',
   entry: {
     app: './src/index.js',
