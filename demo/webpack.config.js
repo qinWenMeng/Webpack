cd24cc3031d7a2bb281728f498ae8f10ae17f5be
qinWenMeng <qwm668@gmail.com>
2019-06-17 17:15:20 +0800

    --outputPath

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -57,21 +57,31 @@ module.exports = {
       {
         test: /\.scss/,
         use: [
-          { loader: 'style-loader' },
-          { loader: 'css-loader' },
-          { loader: 'sass-loader' },
+          { loader: 'style-loader' }, // 将 JS 字符串生成为 style 节点
+          { loader: 'css-loader' }, // 将 CSS 转化成 CommonJS 模块
+          { loader: 'sass-loader' }, // 将 Sass 编译成 CSS，默认使用 Node Sass
         ]
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
         use: [
-          'file-loader',
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'images',
+            },
+          }
         ]
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
         use: [
-          'file-loader',
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'fonts',
+            },
+          }
         ]
       },
     ],
