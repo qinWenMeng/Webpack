19a6aad5d2fa82c1685ec05b12be12654f65f0d4
qinWenMeng <qwm668@gmail.com>
2019-06-18 22:46:37 +0800

    --webpack.prod.js-output

--- demo/webpack.prod.js
+++ demo/webpack.prod.js
@@ -3,4 +3,8 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'production', // 删除的“未引用代码(dead code)” 压缩代码
+  output: {
+    // filename: '[name].[contenthash].js',
+    publicPath: '/',
+  }
 });
